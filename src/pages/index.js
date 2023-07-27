440a743b67e1252fe849db8d28817a61390c2942
Yohan Baes <yohan.baes97@gmail.com>
2023-07-27 21:05:19 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,14 +18,14 @@ export default function Home() {
       <main className={homeStyles.main}>
         <div className={homeStyles.presentation}>
           <h1>{`<Frontend Developer />`}</h1>
-          <p>Hi there! My name is <stron>Yohan</stron>, I am a self-taught developer based in France, and I like to create beautiful interfaces with excellent usability.</p>
+          <p>Hi there! My name is <strong>Yohan</strong>, I am a self-taught developer based in France, and I like to create beautiful interfaces with excellent usability.</p>
           <br />
           <p>Here, you'll find my <strong>work</strong>, my <strong>blog</strong>, and some of my <strong>CSS Art</strong>.</p>
         </div>
         <Image
           className={homeStyles.yohan}
           priority
-          src="/yohanPhoto.png"
+          src="/yohanPics.png"
           height={460}
           width={460}
           alt='mailto link'
