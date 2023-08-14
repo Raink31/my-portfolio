2da4c0b7e9a81b73f3a6f91212db07ee6f13f1b4
Yohan Baes <yohan.baes97@gmail.com>
2023-08-14 15:15:26 +0200

    oui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,14 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import {useEffect, useState} from 'react';
 
 import LayoutNav from '@/components/layoutNav'
-import LayoutFooter from '@/components/layoutFooter'
 import homeStyles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [isVisible, setIsVisible] = useState(false);
-  const repeatedText = new Array(20).fill('FRONTEND DEVELOPER'). join(" ")
 
   useEffect(() => {
     setIsVisible(true)
@@ -21,11 +18,6 @@ export default function Home() {
     <div className={`${homeStyles.container}`}>
       <LayoutNav />
       <main className={`${homeStyles.main} ${isVisible && homeStyles.visible}`}>
-        <section className={homeStyles.banner}>
-          <p className={homeStyles.bannerText}>
-            {repeatedText}
-          </p>
-        </section>
         <section className={homeStyles.section1}>
           <Image
             className={homeStyles.yohanPhoto}
